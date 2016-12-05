e39376479582c379583d394c2eb8c967c8ecbdef
chenjin <chenjin@dongfangfuli.com>
2016-12-05 19:12:31 +0800

    update

---
+++ myHome/app/Consts/Download/DownloadConst.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jim
+ * Date: 2016/12/5
+ * Time: 16:02
+ */
+
+namespace App\Consts\Download;
+
+
+class DownloadConst
+{
+
+}
---
+++ myHome/config/filter.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jim
+ * Date: 2016/12/5
+ * Time: 16:06
+ */
+return [
+    'information_schema',
+    'mysql',
+    'performance_schema',
+    'test'
+];
